306c2e75a42334439aa54c83f927b82ed9c0e56c
Chris Werkheiser <cjwerk@gmail.com>
2020-12-12 12:26:48 -0800

    progress

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 const path = require("path");
 const express = require("express");
-const routes = require("./routes");
-const bodyParser = require("body-parser");
-const layout = require('express-layout');
+const apiRoutes = require("./routes/apiRoutes");
+const htmlRoutes = require("./routes.htmlRoute");
 const app = express();
-
+const PORT = process.env.PORT || 3001;
 
 const middlewares = [express.static(path.join(__dirname, "public"))];
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use("/api", apiRoutes);
+app.use("/", htmlRoutes);
 app.use(middlewares);
+
+app.listen(PORT, () => console.log(`Listening on PORT : ${PORT}`));
