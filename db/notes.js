10f4a2006c9fac96e257bd8955d47f815fc5319c
Chris Werkheiser <cjwerk@gmail.com>
2020-12-13 12:27:32 -0800

    thanks wesley

--- db/notes.js
+++ db/notes.js
@@ -4,35 +4,44 @@ const fs = require("fs");
 const readFileAsyn = util.promisify(fs.readFile);
 const writeFileAsync = util.promisify(fs.writeFile);
 
+
 class Notes {
     constructor() {
-this.idDum = 0;
+        this.idDum = 0;
     }
-        read() {
-            return readFileAsyn("db/db.json", "utf8");
-        }
-        getNotes() {
-            return this.read().then(notes => {
-                let notesArray;
-                try{
-                    notesArray = [].concat(JSON.parse(notes));
-                }
-                catch (err) {
-                    notesArray = [];
-                }
-                return notesArray;
-            })
-        }
+    read() {
+        return readFileAsyn("db/db.json", "utf8");
 
-        addNotes(note) {
-            const { title, text} = note;
-            const newNote = { title, text, id: ++this.idDum}
-            return this.getNotes()
+    }
+    write(note) {
+        return writeFileAsync("db/db.json", JSON.stringify(note))
+    }
+    getNotes() {
+        console.log("get notes")
+        return this.read().then(notes => {
+            console.log(notes)
+            let notesArray;
+            try {
+                notesArray = [].concat(JSON.parse(notes));
+            }
+            catch (err) {
+                notesArray = [];
+            }
+            return notesArray;
+        })
+
+    }
+    addNotes(note) {
+        console.log("add notes");
+        const { title, text } = note;
+        const newNote = { title, text, id: ++this.idDum }
+        return this.getNotes()
             .then(notes => [...notes, newNote])
             .then(updateNotes => this.write(updateNotes))
             .then(() => newNote)
-        }
+
     }
+    
+}
 
-
-    module.exports = new Notes();
+module.exports = new Notes();
